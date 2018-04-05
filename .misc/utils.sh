2f50991758367abdf11127241edc8f221d2ef8d8
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2018-04-05 21:37:10 +0200

    Clear terminal when using util functions

--- .misc/utils.sh
+++ .misc/utils.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 
 slide () {
+  clear
   rst2ansi 0${1}-*.rst
 }
 
 note () {
+  clear
   rst2ansi .0${1}-*.rst
 }
 
 ROOT_FOLDER=$(cd `dirname $0` && pwd)/..
 
 section () {
+  clear
   cd ${ROOT_FOLDER}/0${1}-*
 }
 
 workspace () {
+  clear
   cd ${ROOT_FOLDER}/0${1}-*/workspace
 }
