526de5125bbebfd1a02b4dc8e7502f6a964ef7dc
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2018-04-05 22:10:58 +0200

    Add `help` function in utils

--- .misc/utils.sh
+++ .misc/utils.sh
@@ -22,3 +22,13 @@ task () {
   clear
   rst2ansi ../tasks.rst
 }
+
+help () {
+  echo "The workshop is seperated into numerical sections, numbered 1, 2, 3, and so on."
+  echo
+  echo "Here are the available commands used in order to follow along:"
+  echo "section NUMBER :  Go to workshop section NUMBER."
+  echo "slide NUMBER   :  Show slide NUMBER in the current section."
+  echo "note NUMBER    :  Show the presenter notes for slide NUMBER."
+  echo "task NUMBER    :  Go to workspace tasks for section NUMBER."
+}
