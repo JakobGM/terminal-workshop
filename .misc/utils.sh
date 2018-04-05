e24c73d6ee4e44c7a5ecb31cb73c8b547ae3ed17
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2018-04-05 23:26:58 +0200

    Save the current section in utils

--- .misc/utils.sh
+++ .misc/utils.sh
@@ -1,24 +1,29 @@
 #!/bin/sh
 
+ROOT_FOLDER=$(cd `dirname $0` && pwd)/..
+export ORIGINAL_PS1=$PS1
+
 slide () {
   clear
-  rst2ansi *${1}-*.rst
+  rst2ansi ${SECTION_PATH}/*${1}-*.rst
 }
 
 note () {
   clear
-  rst2ansi .*${1}-*.rst
+  rst2ansi ${SECTION_PATH}/.*${1}-*.rst
 }
 
-ROOT_FOLDER=$(cd `dirname $0` && pwd)/..
 
 section () {
-  cd ${ROOT_FOLDER}/*${1}-*
+  export SECTION=$1
+  export SECTION_PATH=$(echo ${ROOT_FOLDER}/*${1}-*)
+  export PS1="(Section ${SECTION}) ${ORIGINAL_PS1}"
+  cd ${SECTION_PATH}
   clear
 }
 
-task () {
-  cd ${ROOT_FOLDER}/*${1}-*/workspace
+tasks () {
+  cd ${SECTION_PATH}/workspace
   clear
   rst2ansi ../tasks.rst
 }
@@ -29,6 +34,6 @@ help () {
   echo "Here are the available commands used in order to follow along:"
   echo "section NUMBER :  Go to workshop section NUMBER."
   echo "slide NUMBER   :  Show slide NUMBER in the current section."
-  echo "note NUMBER    :  Show the presenter notes for slide NUMBER."
-  echo "task NUMBER    :  Go to workspace tasks for section NUMBER."
+  echo "note NUMBER    :  Show the presenter notes for slide NUMBER in the current section."
+  echo "tasks          :  Go to workspace tasks for section."
 }
